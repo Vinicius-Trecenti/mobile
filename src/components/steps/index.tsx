a5e906309e06343296ea9eb9d598c261ae8c7798
Vinicius-Trecenti <vinitrecenti@gmail.com>
2024-12-10 13:52:36 -0300

    Started project

---
+++ src/components/steps/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import { s } from "./styles"
+
+export default function Steps() { 
+    return (
+        <View style={s.container}>
+            <Text style={s.title}>Veja como funciona:</Text>
+        </View>
+    )
+}
